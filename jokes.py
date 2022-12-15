a02ea113816fa84fe3724f82178ba47eec611be9
Kevin Lazarz <kevin@hafur.com>
2022-12-15 16:16:08 -0500

    Added api support for get_all

--- jokes.py
+++ jokes.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template
-# from flask_restful import Resource, Api
-# from flask_cors import CORS
 import requests
 import random
-# from requests.exceptions import HTTPError
-# import json
+import json
+
 
 app = Flask(__name__)
 #CORS(app)
@@ -16,6 +14,49 @@ def home():
     jr = r.json()
 
     return render_template('index.html', joke = jr['joke'])
+
+@app.route('/all', methods=['GET'])
+def all():
+    baseurl = 'https://g7306646445d18e-jokes.adb.eu-amsterdam-1.oraclecloudapps.com/ords/jokes/jokes/'
+    endpoint = '?offset='
+    offset = 0
+
+    r = requests.get(baseurl + endpoint +str(offset))
+
+    jr = r.json()
+
+    if jr['hasMore']:
+        nextoffset = 25 + int(jr['offset'])
+
+    prevoffset = 0
+
+    return render_template('all.html', html_page_text = jr, nextoffset=nextoffset, prevoffset = prevoffset)
+
+@app.route('/all/<int:offset>', methods=['GET'])
+def offset(offset):
+    baseurl = 'https://g7306646445d18e-jokes.adb.eu-amsterdam-1.oraclecloudapps.com/ords/jokes/jokes/'
+    endpoint = '?offset='
+    offset = offset
+
+    r = requests.get(baseurl + endpoint +str(offset))
+
+    print(baseurl + endpoint +str(offset))
+
+    jr = r.json()
+
+    if jr['hasMore']:
+        nextoffset = 25 + int(jr['offset'])
+    else:
+        nextoffset = 0
+
+    if jr['offset'] > 0:
+        prevoffset = offset - 25
+    else:
+        prevoffset = 0
+
+
+    return render_template('all.html', html_page_text = jr, nextoffset=nextoffset, prevoffset=prevoffset)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
